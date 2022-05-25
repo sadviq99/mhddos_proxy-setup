2b93edf30857e30cb835c742bfc524b1b7f86092
sadviq <sadviq99@gmail.com>
2022-05-25 18:01:31 +0200

    Small adjustments to fix the output

--- tg.sh
+++ tg.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
+# Enable debug mode to see what we send to Telegram
 set -x
 
+# Get billing info only if DO variable set
 if [ ! -z "$DIGITALOCEAN_TOKEN" ]; then
     BAL_RESPONSE=$(curl -s -X GET \
       -H "Content-Type: application/json" \
@@ -14,16 +16,21 @@ fi
 # Static link to targets health checks
 HEALTH_URL="https://itarmy.com.ua/check/"
 
+# Save large output to a file
+LOGS=$(mktemp)
+docker inspect --format='{{.LogPath}}' $(docker compose ps | tail -n -1 | awk '{print $1}') | xargs jq -r -j .log > $LOGS
+
 # Get most important info
-THREADS=$(docker compose logs --tail=1000 | grep -o Threads.\* | tail -n 1 | cut -d' ' -f 2)
-PROXIES=$(docker compose logs --tail=1000 | grep -o Proxies.\* | tail -n 1 | cut -d' ' -f 2)
-TOTAL=$(docker compose logs --tail=1000 | grep -o Total.\* | tail -n 1)
+THREADS=$(grep -o Threads.* $LOGS| tail -n 1 | cut -d' ' -f 2)
+PROXIES=$(grep -o Proxies.* $LOGS | tail -n 1 | cut -d' ' -f 2)
+TOTAL=$(grep -o Total.* $LOGS | tail -n 1)
 CONNECTIONS=$(echo $TOTAL | cut -d' ' -f 3 | sed s/,//)
 REQUESTS=$(echo $TOTAL | cut -d' ' -f 5 | sed s/,//)
 TRAFFIC=$(echo $TOTAL | cut -d' ' -f 7,8)
 
 # Find the top 5 targets by traffic
-ALL_TARGETS=$(docker compose logs --tail=2000 | sed -n '/Threads/,/Total/p' | grep -o Target.\*)
+ALL_TOTALS=$(grep Total $LOGS| wc -l)
+ALL_TARGETS=$(awk -v start=$(($ALL_TOTALS-1)) '/Total/{n++;next};n==start;n==start+1{exit}' $LOGS | grep -o Target.*)
 TARGETS_MB=$(echo "$ALL_TARGETS" | grep 'MB' | cut -d' ' -f 2,13,14 | sed s/,// | sort -u -k2 -n -r | awk -F' ' '!_[$1]++')
 TARGETS_KB=$(echo "$ALL_TARGETS" | grep 'kBit' | cut -d' ' -f 2,13,14 | sed s/,// | sort -u -k2 -n -r | awk -F' ' '!_[$1]++')
 TARGETS_B=$(echo "$ALL_TARGETS" | grep ' Bit' | cut -d' ' -f 2,13,14 | sed s/,// | sort -u -k2 -n -r | awk -F' ' '!_[$1]++')
@@ -50,8 +57,12 @@ message+="$TOP_5_TARGETS"
 
 keyboard="{\"inline_keyboard\":[[{\"text\":\"Open health report\", \"url\":\"${HEALTH_URL}\"}]]}"
 
+# Send to Telegram
 curl -s --data "text=${message}" \
         --data "reply_markup=${keyboard}" \
         --data "chat_id=$TG_CHAT_ID" \
         --data "parse_mode=markdown" \
         "https://api.telegram.org/bot${TG_TOKEN}/sendMessage"
+
+# Remove logs file
+rm $LOGS
